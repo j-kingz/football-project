fd85048f498f603ea3215e910401fb4425ba3ad3
j-kingz <justin.macharia@strathmore.edu>
2024-07-08 10:38:18 +0300

    upgrade

--- sql/db.sql
+++ sql/db.sql
@@ -5,4 +5,14 @@ USE webapp;
 
 -- create table;
 
-
+DROP TABLE IF EXISTS users ;
+CREATE TABLE IF EXISTS users (
+    userId bigint(11) NOT NULL AUTO_INCRMENT,
+    fullname varchar(50) NOT NULL DEFAULT '',
+    username varchar(50) NOT NULL DEFAULT '',
+    email varchar(50) NOT NULL DEFAULT '',
+    password varchar(60) NOT NULL DEFAULT '',
+    userType varchar(30) NOT NULL DEFAULT '',
+    userimage varchar(50) NOT NULL DEFAULT '',
+);
+ 
