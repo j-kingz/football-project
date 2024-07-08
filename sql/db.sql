e36722cb5f195f6493056daf3c82bcdc86fc7816
j-kingz <justin.macharia@strathmore.edu>
2024-07-08 10:56:00 +0300

    upgrade

--- sql/db.sql
+++ sql/db.sql
@@ -6,13 +6,16 @@ USE webapp;
 -- create table;
 
 DROP TABLE IF EXISTS users ;
-CREATE TABLE IF EXISTS users (
+CREATE TABLE IF NOT EXISTS users (
     userId bigint(11) NOT NULL AUTO_INCRMENT,
     fullname varchar(50) NOT NULL DEFAULT '',
     username varchar(50) NOT NULL DEFAULT '',
     email varchar(50) NOT NULL DEFAULT '',
     password varchar(60) NOT NULL DEFAULT '',
     userType varchar(30) NOT NULL DEFAULT '',
-    userimage varchar(50) NOT NULL DEFAULT '',
+    userImage varchar(50) NOT NULL DEFAULT 'userImage. jpg',
+    created bigint(10)NOT NULL DEFAULT 0,
+    updated bigint(10)NOT NULL DEFAULT 0,
+
 );
  
