006c39afb51a3782a3f7f83152a753edebbba051
j-kingz <justin.macharia@strathmore.edu>
2024-07-08 15:31:40 +0300

    upgrade

--- services.php
+++ services.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+<title>Services</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/service.css">
 </head>
